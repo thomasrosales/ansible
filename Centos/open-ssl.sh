939f0ce7174d4f12a6782a5311d768f41e6f8202
Tomás Rosales <tomasjoaquinrosales@gmail.com>
2020-05-17 17:03:10 -0300

    Centos Master Server

---
+++ Centos/open-ssl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing OpenSSL"
+
+curl https://ftp.openssl.org/source/old/1.1.1/openssl-1.1.1.tar.gz | tar -xzv
+yum -y update
+yum -y install make gcc perl pcre-devel zlib-devel
+cd openssl-1.1.1
+./config --prefix=/usr --openssldir=/etc/ssl \
+    --libdir=lib no-shared zlib-dynamic
+make
+make install
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
+echo "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64" >> ~/.bashrc
