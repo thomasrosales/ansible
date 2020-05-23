d1d650b2523567273df4e62aa5bd68e5f9a0b8c9
Tomás Rosales <37603053+thomasrosales@users.noreply.github.com>
2020-05-23 17:00:29 -0300

    Add files via upload

---
+++ Centos/script-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "HOLIS" >> /root/script-test.log
+echo "HOLIS" >> /root/script-test.log
+echo "HOLIS" >> /root/script-test.log
+echo "HOLIS" >> /root/script-test.log
+echo "HOLIS" >> /root/script-test.log
+echo "HOLIS" >> /root/script-test.log
